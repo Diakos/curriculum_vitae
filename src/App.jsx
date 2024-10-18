3fd63cf999ddcbd1358dee057fabe0041a4393b5
Diakos <jdiakos75@gmail.com>
2024-10-19 02:12:27 +0300

    Added google analytics & gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./screens/home/HomeScreen"
 import Error from "./screens/error/ErrorScreen"
 import "./assets/sass/style.scss"
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-R73JP724JQ"); // Replace with your GA4 tracking ID
+
+ReactGA.send("pageview"); // Send a pageview for the first load
 
 const router = createBrowserRouter ([
   {
